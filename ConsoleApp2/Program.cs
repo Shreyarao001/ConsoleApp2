833c1e7d18c697900b796e6244733ad0dcfd72ec
shreya <shreyachinmayi@gmail.com>
2024-02-15 12:05:49 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+Console.WriteLine("Welcome to Authentication App");
+SignIn obj = new SignIn();
+Console.WriteLine(obj.Authenticate("admin", "admin@1256"));
---
+++ ConsoleApp2/SignIn.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName,string password)
+        {
+            string msg;
+            if(string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+            {
+                msg = "details requried";
+            }
+            else
+            {
+                if((userName=="admin") && (password =="admin@1256"))
+                {
+                    msg = "Authentication pass";
+                }
+                else
+                {
+                    msg = "Authentication Fail";
+                
+                }
+
+            }
+            return msg;
+        }
+
+    }
+}
